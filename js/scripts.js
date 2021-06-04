4dd92c2cb7a004ce04e7c7a11cd4866b880ee9d3
professional-pigeon <kyle.kayperez@gmail.com>
2021-06-04 10:13:11 -0700

    add hidden elements style sidebar, create js outline.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+
+
+$(document).ready(function(){
+  $("from#formOne").submit(function(event){
+    let userName = $("#username").val();
+    let interest = $("#interest").val();
+    let langType = $("#langtype").val();
+    let 
+
+  
+  })
+
+
+
+
+
+})
